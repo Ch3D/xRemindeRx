e968ed489fb1d5f9f458a793033ebaeef99e6c90
ch3d <ch3d@axmor.com>
2013-08-09 19:12:27 +0700

    Implement search functionality

--- src/com/ch3d/xreminderx/activity/ReminderSearchResultActivity.java
+++ src/com/ch3d/xreminderx/activity/ReminderSearchResultActivity.java
@@ -1,24 +1,35 @@
 
 package com.ch3d.xreminderx.activity;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
-public class ReminderSearchResultActivity extends Activity {
+import com.ch3d.xreminderx.adapter.RemindersAdapter;
+import com.ch3d.xreminderx.provider.RemindersProvider;
+
+public class ReminderSearchResultActivity extends ListActivity {
 
     private void handleIntent(final Intent intent) {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             final String query = intent.getStringExtra(SearchManager.QUERY);
             // use the query to search your data somehow
+            final Cursor cursor = getContentResolver().query(
+                    Uri.withAppendedPath(RemindersProvider.REMINDERS_SEARCH, query),
+                    null,
+                    null,
+                    null,
+                    null);
+            setListAdapter(new RemindersAdapter(this, cursor, false));
         }
     }
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView()
         handleIntent(getIntent());
     }
 
