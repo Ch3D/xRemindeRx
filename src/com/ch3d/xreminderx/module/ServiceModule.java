694dbf73df7c990da7f0fee3f4fed6cf8ffc684d
ch3d <ch3d@axmor.com>
2014-03-26 12:50:55 +0700

    fix compilation errors

--- src/com/ch3d/xreminderx/module/ServiceModule.java
+++ src/com/ch3d/xreminderx/module/ServiceModule.java
@@ -6,6 +6,6 @@ import com.ch3d.xreminderx.notifications.RingerService;
 import dagger.Module;
 
 @Module(injects = RingerService.class, complete = false)
-public class ServiceModule extends ActivityModule {
+public class ServiceModule {
 
 }
